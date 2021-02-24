49a0eebed0c5fe6c144b3c918b5732f6a031f6f3
Kacper Walter <kacwal@poczta.fm>
2021-02-24 19:36:40 +0100

    Day 1

--- day-1/main.js
+++ day-1/main.js
@@ -1,18 +1,19 @@
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return; // stop the function from running
+    if (!audio) return; // stop the function from running
     audio.currentTime = 0; // rewind to the start
     audio.play();
-    key.classList.add('playing')
-});
-
-const keys = document.querySelectorAll('.key');
-const i = 2;
-
-const iterator = () => {
-    for(let i = 1; i > 10; i++ {
-	
-	}
+    key.classList.add('playing');
 }
 
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;//
+    this.classList.remove('playing');
+}
+
+const keys = [...document.querySelectorAll('.key')];
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
---
+++ day-1/old.js
@@ -0,0 +1,18 @@
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; // stop the function from running
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add('playing')
+});
+
+const keys = document.querySelectorAll('.key');
+const i = 2;
+
+const iterator = () => {
+    for(let i = 1; i > 10; i++ {
+	
+	}
+}
+
